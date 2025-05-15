27e69f8b15b2ac9417fad97d5861c73d94cab695
Ikhsan <pratamaikhsan974.com>
2025-05-15 20:41:19 +0800

    perbaiki db

--- db.php
+++ db.php
@@ -1,11 +1,14 @@
 <?php
-$host = "e0k4s0wgocc00s0gs8c8oc80"; // dari MySQL URL
-$user = "mysql";
-$pass = "comsmakda";
-$dbname = "multi-role"; // PENTING! Harus persis sama
+$host = 'e0k4s0wgocc00s0gs8c8oc80';
+$user = 'comsmkda';
+$password = 'comsmakda';
+$database = 'multi-role';
 
-$conn = new mysqli($host, $user, $pass, $dbname);
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
+
+
