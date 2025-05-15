08546e3b38f07c825b0a6cc4d8eb25260e875de8
Ikhsan <pratamaikhsan974.com>
2025-05-15 18:39:04 +0800

    perbaiki login

--- db.php
+++ db.php
@@ -1,11 +1,10 @@
 <?php
-$host = "e0k4s0wgocc00s0gs8c8oc80";
+$host = "e0k4s0wgocc00s0gs8c8oc80"; // dari MySQL URL
 $user = "mysql";
 $pass = "comsmakda";
-$dbname = "multi-role";
+$dbname = "multi-role"; // PENTING! Harus persis sama
 
 $conn = new mysqli($host, $user, $pass, $dbname);
-
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
